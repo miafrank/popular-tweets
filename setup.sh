19740ca6ffa09722545e726536d99dc824e2ba9b
Mia Frank <31712634+miafrank@users.noreply.github.com>
2020-05-11 14:16:29 -0500

    added setup script

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Used to run kafka producer for certain amount of time
+function timeout() { perl -e 'alarm shift; exec @ARGV' "$@"; }
+
+# Start Kafka, Zookeeper, and Schema Registry.
+confluent local start
+# Configure Hbase to read from Kafka topic
+confluent local load hbase -- -d hbase-avro.json
+
+# Run Kafka Topic
+timeout 30 mvn exec:java -Dexec.mainClass=com.github.miafrank.twitter.TwitterProducer
+# Run Kafka Stream that applies a filter to messages from TwitterProducer
+timeout 60 exec:java -Dexec.mainClass=com.github.miafrank.twitter.TwitterStream
+
